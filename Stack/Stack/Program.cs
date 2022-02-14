4face59e41508d7637948c4db92ad0461f8dd36c
Sikander <sikku.rk405@gmail.com>
2022-02-14 08:31:23 +0530

    Added Stack program

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();         
+            stack.Pop();
+            stack.Display();
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Stack
+    {
+        public Node head;
+      
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.head;
+            }
+            this.head = node;
+            Console.WriteLine(data + " pushed to stack.");
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        public void Pop()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine(this.head.data + " poped from the stack");
+            this.head = this.head.next;
+        }
+
+       
+    }
+}
