f7f37271a27784088388c941b64a8070fcc7f169
Sikander <sikku.rk405@gmail.com>
2022-02-14 17:08:40 +0530

    Added Queue program

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+
+            queue.Enqueue(70);
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();           
+            queue.Display();
+        }
+    }
+}
---
+++ Queue/Queue/Queue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    internal class Queue
+    {
+        public Node head;
+        public Node Enqueue(int data)
+        {
+            Node newnode = new Node(data);
+            if (head == null)
+            {
+                head = newnode;
+                return head;
+            }
+            else
+            {
+                Node Temp = head;
+                while (Temp.next != null)
+                {
+                    Temp = Temp.next;
+                }
+                Temp.next = newnode;
+                return head;
+            }
+        }
+        public Node Dequeue()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is  Empty");
+                return head;
+            }
+            else
+            {
+                Console.WriteLine(head.data + ": this is Dequed from Queue");
+                head = head.next;
+                return head;
+            }
+        }
+
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
